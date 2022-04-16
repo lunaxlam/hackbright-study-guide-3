278ab409711415ee4f1066770527b5f1af5549c3
lunaxlam@gmail.com <lunalam@Lunas-MBP.attlocal.net>
2022-04-16 15:55:56 -0700

    Completed server.py

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -16,16 +16,30 @@ def show_homepage():
 #                             #
 ###############################
 
+@app.route('/save-name')
+def save_name():
+    """Save the name into the 'name' session key."""
+
+    name = request.args.get("name")
+
+    session["name"] = name
+
+    return redirect('/')
+
 
 @app.route('/form')
 def show_form():
     """Show form with message options."""
-    pass
+    return render_template('form.html')
 
 @app.route('/results')
 def show_results():
     """Show resulting message."""
-    pass
+
+    message = request.args.get("message")
+    print(message)
+
+    return render_template('results.html', message=message)
 
 
 if __name__ == "__main__":
